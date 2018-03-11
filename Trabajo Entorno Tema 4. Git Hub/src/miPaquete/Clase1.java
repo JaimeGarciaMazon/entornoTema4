f0623959e5a6de0a7235cb2f81655d95a1f89167
Juanlu-PC <Juanlu-PC@DESKTOP-DT04595>
2018-03-11 20:12:14 +0100

    Commit Ejercicio 3

--- Trabajo Entorno Tema 4. Git Hub/src/miPaquete/Clase1.java
+++ Trabajo Entorno Tema 4. Git Hub/src/miPaquete/Clase1.java
@@ -2,14 +2,15 @@ package miPaquete;
 
 public class Clase1 {
 	String cadena;
-	String cadena2;
+	String cadenas;
+	String cadena3;
 
 	public String getCadena2() {
-		return cadena2;
+		return cadenas;
 	}
 
 	public void setCadena2(String cadena2) {
-		this.cadena2 = cadena2;
+		this.cadenas = cadena2;
 	}
 
 	public String getCadena() {
