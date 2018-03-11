10787aad699bc5a5815080cedbe3f6b7a6e0b0d0
Juanlu-PC <Juanlu-PC@DESKTOP-DT04595>
2018-03-11 19:15:24 +0100

    Segundo Commit

--- Trabajo Tema 5 ED/src/miPaquete/MiClase.java
+++ Trabajo Tema 5 ED/src/miPaquete/MiClase.java
@@ -1,5 +1,13 @@
 package miPaquete;
 
 public class MiClase {
-	
+	private int nuevoAtributo;
+
+	public int getNuevoAtributo() {
+		return nuevoAtributo;
+	}
+
+	public void setNuevoAtributo(int nuevoAtributo) {
+		this.nuevoAtributo = nuevoAtributo;
+	}
 }
