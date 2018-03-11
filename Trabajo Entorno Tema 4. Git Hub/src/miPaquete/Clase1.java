5816316a8957a67022a64d7e512095dab4c417fc
Juanlu-PC <Juanlu-PC@DESKTOP-DT04595>
2018-03-11 20:02:30 +0100

    Commit 3

--- Trabajo Entorno Tema 4. Git Hub/src/miPaquete/Clase1.java
+++ Trabajo Entorno Tema 4. Git Hub/src/miPaquete/Clase1.java
@@ -4,6 +4,14 @@ public class Clase1 {
 	String cadena;
 	String cadena2;
 
+	public String getCadena2() {
+		return cadena2;
+	}
+
+	public void setCadena2(String cadena2) {
+		this.cadena2 = cadena2;
+	}
+
 	public String getCadena() {
 		return cadena;
 	}
