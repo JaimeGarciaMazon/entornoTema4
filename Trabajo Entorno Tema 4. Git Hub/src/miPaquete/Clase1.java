a85f72ebe5ec22c6917099d333bbadc92ec3c64a
juanlu-rr <37272847+juanlu-rr@users.noreply.github.com>
2018-03-11 20:24:40 +0100

    Update Clase1

--- Trabajo Entorno Tema 4. Git Hub/src/miPaquete/Clase1.java
+++ Trabajo Entorno Tema 4. Git Hub/src/miPaquete/Clase1.java
@@ -1,25 +1,6 @@
 package miPaquete;
 
 public class Clase1 {
-	String cadena;
-	String cadenas;
-	String cadena3;
-
-	public String getCadena2() {
-		return cadenas;
-	}
-
-	public void setCadena2(String cadena2) {
-		this.cadenas = cadena2;
-	}
-
-	public String getCadena() {
-		return cadena;
-	}
-
-	public void setCadena(String cadena) {
-		this.cadena = cadena;
-	}
-	
-
+	String ejercicio4;
+	String ejemplo4; 
 }
